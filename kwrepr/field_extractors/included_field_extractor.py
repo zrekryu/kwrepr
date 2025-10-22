26e89f16e2c7a178b24007fc53a81ec83acad5b7
Zrekryu <145272593+zrekryu@users.noreply.github.com>
2025-10-22 11:14:58 +0530

    Update included_field_extractor.py

--- kwrepr/field_extractors/included_field_extractor.py
+++ kwrepr/field_extractors/included_field_extractor.py
@@ -1,8 +1,9 @@
 from collections.abc import Callable, Iterator, Mapping, Sequence
 from typing import Any
 
+from kwrepr.types import Instance
+
 from .base_field_extractor import BaseFieldExtractor
-from ..types import Instance
 
 
 class IncludedFieldExtractor(BaseFieldExtractor):
